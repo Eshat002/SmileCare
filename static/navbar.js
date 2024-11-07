905bc11a3fea5dbeab3faa9fb687d75358152fff
eshat002 <eshatjubayer22@outlook.com>
2024-11-07 20:38:18 +0600

    fix tailwind settings

--- static/navbar.js
+++ static/navbar.js
@@ -2,3 +2,15 @@ function toggleMobileMenu() {
   const mobileMenu = document.getElementById("mobile-menu");
   mobileMenu.classList.toggle("hidden");
 }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const links = document.querySelectorAll("ul li a");
+//   links.forEach((link) => {
+//     link.addEventListener("click", function () {
+//       // Remove active class from all links
+//       links.forEach((link) => link.classList.remove("active-link"));
+//       // Add active class to clicked link
+//       this.classList.add("active-link");
+//     });
+//   });
+// });

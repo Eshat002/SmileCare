0b456c7b6f59269bf143b40c84086f6fbfde3f7e
eshat002 <eshatjubayer22@outlook.com>
2024-12-25 20:28:08 +0600

    Add style and validation to contact form

--- home/forms.py
+++ home/forms.py
@@ -1,22 +1,30 @@
 from django import forms
 from .models import Contact
+from django.core.exceptions import ValidationError
+import re
+
 
 # model form
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'phone']
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'placeholder': 'Enter Your Name',
-                'class': 'form-input',
-            }),
-            'phone': forms.TextInput(attrs={
-                'placeholder': 'Enter Your Phone Number',
-                'class': 'form-input',
-                'pattern': r'(\+?\d{1,4}[\s\-]?)?(\(?\d{1,4}\)?[\s\-]?)?\d{1,4}[\s\-]?\d{1,4}[\s\-]?\d{1,4}',
-            }),
-        }
+        # widgets = {
+        #     'name': forms.TextInput(attrs={
+        #         'placeholder': 'Enter Your Name',
+        #         'class': 'form-input',
+        #     }),
+        #     'phone': forms.TextInput(attrs={
+        #         'placeholder': 'Enter Your Phone Numberrrr',
+        #         'pattern': r'^\+?[0-9\s\-()]*$',
+        #      }),
+        # }
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if not re.match(r'^\+?[0-9\s\-()]*$', phone):
+            raise ValidationError('Invalid phone number format.')
+        return phone
 
 
 #regular form
